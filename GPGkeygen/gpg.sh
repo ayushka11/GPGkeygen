e306cf9a812a2d0ec5e9449b6c26a70040211c84
ayushka11 <gargayushka@gmail.com>
2024-03-19 04:12:30 +0530

    finally!
    Signed-off-by: ayushka11 <gargayushka@gmail.com>

---
+++ GPGkeygen/gpg.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+source utils.sh
+
+BLACK="\e[30m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[37m"
+BLUE="\e[34m"
+MAGENTA=" \e[35m"
+CYAN="\e[36m"
+WHITE="\e[37m"
+RESET="\e[0m"
+echo -e "${WHITE}-----WELCOME TO GPG KEY MANAGER------${RESET}"
+exit_var=0
+while [ $exit_var -eq 0 ];
+do
+    key_exists
+    if [ $keys -eq 0 ];
+    then
+        echo -e "${GREEN} There are no keys associated with this account. Do you wish to: ${RESET}"
+        echo -e "${RED} 0. Exit. ${RESET}"
+        echo -e "${YELLOW} 1. Set up a new key. ${RESET}"
+    else 
+        echo -e "${GREEN} There are ${keys} keys associated with this account. Do you wish to: ${RESET}"
+        echo -e "${RED} 0. Exit. ${RESET}"
+        echo -e "${YELLOW} 1. Set up a new key. ${RESET}"
+        echo -e "${CYAN} 2. Work with an existing key. ${RESET}"
+        echo -e "${WHITE} 3. Delete a key. ${RESET}"
+        echo -e "${BLUE} 4. View existing keys. ${RESET}"
+    fi
+    read -p "enter the corresponding number : " num
+    if [ $num -eq 1 ];
+    then
+        key_exists
+        gpg --full-generate-key
+        newkey = ${key[keys]}
+        work_key ${newkey}
+    elif [ $num -eq 2 ];
+    then
+        listkeys
+        read numkey
+        existkey=${key[keys-numkey+1]}
+        work_key ${existkey}
+    elif [ $num -eq 3 ];
+    then
+        listkeys
+        read delkey
+        exkey=${key[keys-delkey+1]}
+        echo $exkey
+        del_key ${exkey}
+    elif [ $num -eq 4 ];
+    then
+        listkeys
+    else
+        exit
+        ((exit_var++))
+    fi
+done
+    
+
+
---
+++ GPGkeygen/utils.sh
@@ -0,0 +1,69 @@
+declare -a key
+
+key_exists() {
+    keyno=$(gpg --list-secret-keys --keyid-format=long|awk /sec/|wc -l)
+    keys=$keyno
+    keys2=$keyno
+    while [ $keyno -gt 0 ];
+    do
+        key[keyno]=$(gpg --list-secret-keys --keyid-format=long|awk /sec/|cut -b 15-30| sed "${keyno}q;d")
+        ((keyno--))
+    done
+}
+
+exit() {
+    echo -e "${WHITE}------THANK YOU FOR USING GPGKEYGEN------${RESET}"
+}
+
+listkeys() {
+    echo -e "${GREEN}Which key would you like to use?${RESET}"
+    while [ $keys2 -gt 0 ];
+    do
+        index=$(($keys+1-$keys2))
+        echo -e "${CYAN} $index : "${key[keys2]}" ${RESET}"
+        ((keys2--))
+    done
+}
+
+commit() {
+    inpc=$1
+    gpg --armor --export $inpc
+    echo -e "${WHITE}Add the key to your github account.${RESET}"
+    echo -e "${WHITE}~go to your github account settings.${RESET}"
+    echo -e "${WHITE}~in the access section, click ssh and gpg keys.${RESET}"
+    echo -e "${WHITE}~Next to the "GPG keys" header, click New GPG key.${RESET}"
+    echo -e "${WHITE}~fill the details as said.${RESET}"
+    echo -e "${WHITE}~click add gpg key.${RESET}"
+    echo
+    echo -e "${GREEN}Do you wish to use this key: ${RESET}"
+    echo -e "${RED}0. Exit.${RESET}"
+    echo -e "${CYAN}1. Locally.${RESET}"
+    echo -e "${CYAN}2. Globaly.${RESET}"
+    read arg
+    if [ $arg -eq 1 ];
+    then
+        git config user.signingkey $inpc
+    else
+        git config --global user.signingkey $inpc
+    fi
+}
+
+work_key() {
+    inp="$1"
+    gpg --armor --export $inp
+    echo -e "${GREEN}What do you want to do with this key?${RESET}"
+    echo -e "${RED}0. Exit.${RESET}"
+    echo -e "${CYAN}1. Use it for your commits.${RESET}"
+    read work
+    if [ $work -eq 1 ];
+    then
+        commit $inp
+    fi
+}
+
+del_key() {
+    inpd=$1
+    echo "$inpd"
+    gpg --delete-secret-key $inpd
+    gpg --delete-key $inpd
+}
